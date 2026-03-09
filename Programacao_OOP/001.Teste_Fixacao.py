8f2e51852560f867f1788cd8adfcbf5e2f5fc514
RafaLNunes <rafaf.code@gmail.com>
2026-03-09 11:16:18 -0300

    Update 001.Teste_Fixacao.py

--- Programacao_OOP/001.Teste_Fixacao.py
+++ Programacao_OOP/001.Teste_Fixacao.py
@@ -10,12 +10,15 @@ def main():
     Fases = ["A", "B", "C"]
     nominais = [127.0, 127.0, 127.0]
     corrente = 10.5
-
+    F_val = "Fases"
+    v_real_val = "Volts"
+    p_Ativa_val = "Potencia"
+    print(f"{F_val:<6} | {v_real_val:<11} | {p_Ativa_val:<11}")
     for F,N in zip(Fases,nominais):
         v_real = Gerar_leitura(N)
         p_Ativa = Calc_Pot(v_real, corrente)
 
-        print(f"{F:<5} | {v_real:>12.2f} | {p_Ativa:>12.2f}")
+        print(f"{F:<6} | {v_real:>11.2f} | {p_Ativa:>11.2f}")
 
 if __name__ == "__main__":
     main()
