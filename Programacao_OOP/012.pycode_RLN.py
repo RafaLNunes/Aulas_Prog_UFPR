a1ea00d53c351abda5a16efe6d818e9f4f5977e6
RafaLNunes <rafaf.code@gmail.com>
2026-03-11 10:32:52 -0300

    Create 012.pycode_RLN.py
    Testando Criação de Branchs

---
+++ Programacao_OOP/012.pycode_RLN.py
@@ -0,0 +1,34 @@
+import numpy as np
+import time
+import random as rd
+import panda as pd
+
+def exibir_matrix(matrix):
+    print("\n\n --------------- MATRIX ESTRUCTURE ---------------")
+    for line in matrix:
+
+        for colun in line:
+            print(f"{colun:^5}", end = " ")
+        print()
+
+def rand_texture():
+    #aqui vem outra
+
+    line = int(input("N° de Linhas: "))
+    colu = int(input("N° de Colunas: "))
+
+    matrix = []
+    for i in range(line):
+        new_line = []
+        for j in range(colu):
+            val = rd.randint(-150,250)
+            print(f" Valor para [{i}][{j}]:[{val}]")
+            new_line.append(val)
+        matrix.append(new_line)
+    # exibir_matrix(matrix)
+
+def main():
+    # aqui vem a central
+
+if __name__ == "__main__":
+    main()
