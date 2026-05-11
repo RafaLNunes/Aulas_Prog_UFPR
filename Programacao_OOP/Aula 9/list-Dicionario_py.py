874eb1b07809375eb769154ddde1572f4ef0e9e1
RafaLNunes <rafaf.code@gmail.com>
2026-05-11 10:45:34 -0300

    Lista nova

--- Programacao_OOP/Aula 9/list-Dicionario_py.py
+++ Programacao_OOP/Aula 9/list-Dicionario_py.py
@@ -1,3 +1,15 @@
+from collections import UserList
+import numpy as np
+
+class Lista_Sensor(UserList):
+    def append(self, item):
+        if item <0:
+            raise ValueError("Erro de QTD")
+        super().append(item)
+
+
+
+
 def main():
     l = [12,2,3,4,5,1,6,7,17,8,9,10,11,13,14,15,16,18,19,20]
     s = set(l)
@@ -12,6 +24,12 @@ def main():
     print(a | b)
     print(a & b)
     print(a - b)
+
+    print("\n\n\n\n")
+    
+    let = Lista_Sensor()
+    let.append(10.5)
+
     
 if __name__ == "__main__":
     main()
---
+++ Programacao_OOP/Listas/Lista 2/Lista_03.py
@@ -0,0 +1,6 @@
+def main():
+    print ("Ex 1:\n")
+    
+    
+    if __name__ == "__main__":
+        main()
