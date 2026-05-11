89cf455f1e628bf9808cc15c864f01280df5d6c1
RafaLNunes <rafaf.code@gmail.com>
2026-05-11 10:52:07 -0300

    Update Lista_03.py

--- Programacao_OOP/Listas/Lista 2/Lista_03.py
+++ Programacao_OOP/Listas/Lista 2/Lista_03.py
@@ -1,5 +1,20 @@
+
+# ex 1
+class Sensor_ex1():
+    def __init__(self, id, tipo, leitura):
+        self.id = id
+        self.tipo = tipo
+        self.let = leitura
+        
+class Estacao_ex1:
+    def __init__(self, sensor_list):
+        self.sensores = sensor_list
+    def add_sens(sensor):
+        
+
 def main():
     print ("Ex 1:\n")
+    
     
     
     if __name__ == "__main__":
