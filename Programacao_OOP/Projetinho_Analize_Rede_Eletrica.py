e80bbc795f5dbd42c80782552331c5f3fd0f5fdf
RafaLNunes <rafaf.code@gmail.com>
2026-06-03 10:13:30 -0300

    Update Projetinho_Analize_Rede_Eletrica.py

--- Programacao_OOP/Projetinho_Analize_Rede_Eletrica.py
+++ Programacao_OOP/Projetinho_Analize_Rede_Eletrica.py
@@ -1,2 +1,6 @@
 import pandas as pd
 import numpy as np
+# https://scikit-learn.org/stable/index.html
+from sklearn. import  as 
+# https://docs.python.org/3/library/urllib.request.html#urllib.request.Request.full_url
+import urllib.request as request
