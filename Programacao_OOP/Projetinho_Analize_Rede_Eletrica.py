f7534b431ac8bcc0ea59838f3c2b17f0adfcfe25
RafaLNunes <rafaf.code@gmail.com>
2026-06-03 10:29:53 -0300

    Update Projetinho_Analize_Rede_Eletrica.py

--- Programacao_OOP/Projetinho_Analize_Rede_Eletrica.py
+++ Programacao_OOP/Projetinho_Analize_Rede_Eletrica.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 # https://scikit-learn.org/stable/index.html
-from sklearn. import  as 
+from sklearn.ensemble import RandomForestClassifier as rfc
 # https://docs.python.org/3/library/urllib.request.html#urllib.request.Request.full_url
-import urllib.request as request
+import requests as rqt
+
+
+
